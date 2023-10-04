e2cfa6710300af208fccd162f4a0ff2b2804d4fe
pinwc4 <pinwc4@flipsidehobbies.com>
2023-10-03 20:18:56 -0500

    removed typo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/telerrfieldcentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/telerrfieldcentric.java
@@ -25,7 +25,7 @@ public class telerrfieldcentric extends LinearOpMode {
 
         // We want to turn off velocity control for teleop
         // Velocity control per wheel is not necessary outside of motion profiled auto
-        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);+
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         waitForStart();
 
