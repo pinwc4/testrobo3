e9334327eb446609e49793b72c1ce93eb6e76be0
pinwc4 <pinwc4@flipsidehobbies.com>
2023-10-07 18:23:46 -0500

    Updated centerstate picture and path

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -4,6 +4,13 @@ import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
@@ -14,24 +21,23 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(70, 70, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-67.00, -40.00, Math.toRadians(0)))
-
-                                .splineToSplineHeading(new Pose2d(-14.75, -42.64, Math.toRadians(60.00)), Math.toRadians(55.00))
-                                .splineToSplineHeading(new Pose2d(-4.77, -1.69, Math.toRadians(60.00)), Math.toRadians(90.00))
-                                .splineToSplineHeading(new Pose2d(14.46, 18.72, Math.toRadians(60)), Math.toRadians(55))
-                                .splineToSplineHeading(new Pose2d(35.00, 40.00, Math.toRadians(90.00)), Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(-63.00, -40.00, Math.toRadians(0.00)))
+                                .splineTo(new Vector2d(-20.00, -40.00), Math.toRadians(-0.89))
+                                .splineTo(new Vector2d(9.95, 12.00), Math.toRadians(60.00))
+                                .splineToSplineHeading(new Pose2d(37.00, 46.60, Math.toRadians(90.00)), Math.toRadians(50.00))
                                 .build()
                 );
-/*
-        TrajectorySequence hometoboard = drive.trajectorySequenceBuilder(new Pose2d(-67.00, -40.00, Math.toRadians(180)))
-                .splineToSplineHeading(new Pose2d(-14.75, -42.64, Math.toRadians(60.00)), Math.toRadians(235.00))
-                .splineToSplineHeading(new Pose2d(-4.77, -1.69, Math.toRadians(60.00)), Math.toRadians(270.00))
-                .splineToSplineHeading(new Pose2d(14.46, 18.72, Math.toRadians(45.43)), Math.toRadians(235))
-                .splineToSplineHeading(new Pose2d(35.00, 40.00, Math.toRadians(90.00)), Math.toRadians(240))
-                .build();
-*/
-        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+
+
+        Image img = null;
+        try {
+            img = ImageIO.read(new File("MeepMeepTesting/src/main/java/com/example/meepmeeptesting/centerstagerotated.png"));
+        } catch (IOException e) {
+        }
+
+        //meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 //.setDarkMode(true)
+        meepMeep.setBackground(img)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot2)
                 .start();
