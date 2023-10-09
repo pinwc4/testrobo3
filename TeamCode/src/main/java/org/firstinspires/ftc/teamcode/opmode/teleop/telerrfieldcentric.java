d2d5622a65fff5a018eef6d924b60f0c21950316
pinwc4 <pinwc4@flipsidehobbies.com>
2023-10-09 14:48:37 -0500

    Opmode version of field centric

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/telerrfieldcentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/telerrfieldcentric.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.outoftheboxrobotics.photoncore.Photon;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -29,6 +30,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
  */
 @TeleOp(group = "rr field centric")
 public class telerrfieldcentric extends LinearOpMode {
+
     @Override
     public void runOpMode() throws InterruptedException {
         // Initialize SampleMecanumDrive
@@ -52,6 +54,7 @@ public class telerrfieldcentric extends LinearOpMode {
         navxMicro = hardwareMap.get(NavxMicroNavigationSensor.class, "navx");
         navxgyro = (IntegratingGyroscope)navxMicro;
 
+
         ElapsedTime timer = new ElapsedTime();
         timer.reset();
         while (navxMicro.isCalibrating())  {
@@ -65,14 +68,19 @@ public class telerrfieldcentric extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive() && !isStopRequested()) {
+            double starttime = timer.milliseconds();
             // Read pose
             Pose2d poseEstimate = drive.getPoseEstimate();
+
+            float gamely = gamepad1.left_stick_y;
+            float gamelx = gamepad1.left_stick_x;
+            float gamerx = gamepad1.right_stick_x;
 
             // Create a vector from the gamepad x/y inputs
             // Then, rotate that vector by the inverse of that heading
             Vector2d input = new Vector2d(
-                    -gamepad1.left_stick_y,
-                    -gamepad1.left_stick_x
+                    -(gamely * Math.abs(gamely)),
+                    -(gamelx * Math.abs(gamelx))
             ).rotated(-poseEstimate.getHeading());
 
             // Pass in the rotated input + right stick value for rotation
@@ -81,7 +89,7 @@ public class telerrfieldcentric extends LinearOpMode {
                     new Pose2d(
                             input.getX(),
                             input.getY(),
-                            -gamepad1.right_stick_x
+                            -(gamerx * Math.abs(gamerx))
                     )
             );
             Orientation angles = navxgyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
@@ -91,7 +99,9 @@ public class telerrfieldcentric extends LinearOpMode {
             // Update everything. Odometry. Etc.
             drive.update();
 
+            double elapsedtime = timer.milliseconds() - starttime;
             // Print pose to telemetry
+            telemetry.addData("loop ms", elapsedtime);
             telemetry.addData("x", poseEstimate.getX());
             telemetry.addData("y", poseEstimate.getY());
             telemetry.addData("heading", Math.toDegrees(poseEstimate.getHeading()));
