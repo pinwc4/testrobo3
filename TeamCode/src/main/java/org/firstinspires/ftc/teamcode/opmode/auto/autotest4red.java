6793c13e9726e3a79097ec4da134ca0af9839b9c
pinwc4 <pinwc4@flipsidehobbies.com>
2023-10-10 14:09:53 -0500

    Made another example auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/autotest4red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/auto/autotest4red.java
@@ -12,24 +12,36 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.hardware.Robot;
+import org.firstinspires.ftc.teamcode.roadrunner.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.roadrunner.trajectorysequence.TrajectorySequence;
-@Autonomous(name = "Test 3 Red")
-public class autotest3red extends OpMode {
+import com.acmerobotics.roadrunner.trajectory.constraints.MecanumVelocityConstraint;
+
+@Autonomous(name = "Test 4 Red")
+public class autotest4red extends OpMode {
     public Robot robot;
     public Pose2d startPose;
+    public MecanumVelocityConstraint fastModeVel;
     public TrajectorySequence hometoboard;
+    public TrajectorySequence boardtosomewhere;
 
     @Override
     public void init() {
         robot = new Robot(hardwareMap);
         startPose = new Pose2d(-63, -40, Math.toRadians(0));
 
+        fastModeVel = new MecanumVelocityConstraint(60, DriveConstants.TRACK_WIDTH);
+
+
         hometoboard = robot.drive.trajectorySequenceBuilder(startPose)
+                .setVelConstraint(fastModeVel)
                 .splineTo(new Vector2d(-24.00, -40.00), Math.toRadians(0.00))
                 .splineToConstantHeading(new Vector2d(6, 12.00), Math.toRadians(50.00))
                 .splineToSplineHeading(new Pose2d(37.00, 45, Math.toRadians(90.00)), Math.toRadians(50.00))
                 .build();
 
+        boardtosomewhere = robot.drive.trajectorySequenceBuilder(hometoboard.end())
+                .lineToConstantHeading(new Vector2d(60, 20))
+                .build();
     }
 
     /*
@@ -44,6 +56,7 @@ public class autotest3red extends OpMode {
         robot.drive.setPoseEstimate(startPose);
         robot.drive.followTrajectorySequence(hometoboard);
         robot.drive.update();
+        robot.drive.followTrajectorySequenceAsync(boardtosomewhere);
     }
 
     @Override
