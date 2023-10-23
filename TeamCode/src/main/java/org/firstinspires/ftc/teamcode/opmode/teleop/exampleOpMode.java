36476e5882cb65c1d4df2cbfbf8b937a9d440548
pinwc4 <pinwc4@flipsidehobbies.com>
2023-10-23 18:21:13 -0500

    example op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/exampleOpMode.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Example OpMode")
+public class exampleOpMode extends OpMode {
+    public void init(){
+
+    }
+//    public void init_loop(){
+//
+//    }
+//    public void start() {
+//
+//    }
+    public void loop(){
+
+    }
+//    public void stop() {
+//
+//    }
+}
