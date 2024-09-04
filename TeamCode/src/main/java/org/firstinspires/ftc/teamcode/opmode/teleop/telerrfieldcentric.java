5c9ba351aae8f1f2ed66c7ca2774c570ef4bd2f1
pinwc4 <pinwc4@flipsidehobbies.com>
2024-09-04 09:43:30 -0500

    Remove unused broken import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/telerrfieldcentric.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/telerrfieldcentric.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.opmode.teleop;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.outoftheboxrobotics.photoncore.Photon;
+//import com.outoftheboxrobotics.photoncore.Photon;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
